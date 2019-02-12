9686349297bb0a999d6fbd6d181a675ba06d97f6
Nadi Najafabadi <s.nadi@uu.nl>
2019-02-12 14:22:20 +0100

    Update de_preprocessing.py

--- eBay_SClassifier/de_preprocessing.py
+++ eBay_SClassifier/de_preprocessing.py
@@ -7,23 +7,32 @@ import tqdm
 from spellchecker import SpellChecker
 
 
-
 pd.set_option('display.expand_frame_repr', False)
 
 
-def cleanup_doc(corpus):
+def cleanup_document(corpus):
+        # remove lines without comments
+        removed_no_comment = corpus[corpus['FeedbackComment'] != "no comment"]
+        # change FeedbackStatus field to some meaningful values. 0 -> negative,  1 -> positive, 9-> neutral
+        removed_no_comment.loc[removed_no_comment.FeedbackStatus == 0, 'FeedbackStatus'] = "negative"
+        removed_no_comment.loc[removed_no_comment.FeedbackStatus == 1, 'FeedbackStatus'] = "positive"
+        removed_no_comment.loc[removed_no_comment.FeedbackStatus == 9, 'FeedbackStatus'] = "neutral"
+        return removed_no_comment
+
+
+def pre_process(corpus):
 
     # correct expanding contractions
-    # replacer = RegexpReplacer()
-    # corpus['removed_punctuation'] = corpus.apply(lambda x: replacer.replace(x['FeedbackComment']), axis=1)
-    # # print(corpus)
-    #
-    # # correct repeating characters
-    # repeater = RepeatReplacer()
-    # corpus['removed_repetition'] = corpus.apply(lambda y: repeater.repeat(y['removed_punctuation']), axis=1)
+    replacer = RegexpReplacer()
+    corpus['removed_punctuation'] = corpus.apply(lambda x: replacer.replace(x['FeedbackComment']), axis=1)
     # print(corpus)
 
-    # correct spelling
+    # correct repeating characters
+    repeater = RepeatReplacer()
+    corpus['removed_repetition'] = corpus.apply(lambda y: repeater.repeat(y['removed_punctuation']), axis=1)
+    print(corpus)
+
+    # # correct spelling
     # spell = SpellChecker(language='de')
     # misspelled = spell.unknown(['somthing', 'is', 'hapenning', 'hee'])
     # for word in misspelled:
@@ -32,7 +41,6 @@ def cleanup_doc(corpus):
     #
     #     # Get a list of `likely` options
     #     print(spell.candidates(word))
-    #
 
     # Remove stop words
     stop_set = stopwords.words('german')
@@ -48,6 +56,8 @@ def cleanup_doc(corpus):
     # convert all characters to lowercase
     corpus['FeedbackComment_lowercase'] = corpus['FeedbackComment_without_punctuations'].str.lower()
     print(corpus)
+
+    return corpus
 
 
 
