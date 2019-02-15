d9d07d89f5a3ab3c8a231f72cec47c3558caa83b
Nadi Najafabadi <s.nadi@uu.nl>
2019-02-15 17:17:05 +0100

    Update eBaySC.py

--- eBay_SClassifier/eBaySC.py
+++ eBay_SClassifier/eBaySC.py
@@ -7,7 +7,7 @@ pd.set_option('display.expand_frame_repr', False)
 
 # read stat data set chunks into memory
 data_set = pd.read_stata("data/ebay2_indegrees_comments.dta", chunksize=1000000,
-                         columns=['FeedbackID', 'FeedbackComment', 'FeedbackStatus'])
+                         columns=['FeedbackID', 'FeedbackComment', 'FeedbackValue'])
 for chunk in data_set:
     # corpus = pd.DataFrame(chunk['FeedbackStatus'])
     corpus = de_preprocessing.cleanup_document(chunk)
