a03f228e5eff417c87f3f45eff659f6e192be7b9
Nadi Najafabadi <s.nadi@uu.nl>
2019-02-15 17:16:56 +0100

    Update clean.py

--- eBay_SClassifier/clean.py
+++ eBay_SClassifier/clean.py
@@ -3,20 +3,32 @@ import pandas as pd
 
 pd.set_option('display.expand_frame_repr', False)
 
-read = pd .read_stata("data/ebay2_indegrees_comments.dta", chunksize=100, columns=['FeedbackID', 'FeedbackComment', 'FeedbackStatus'])
-for row in read:
-    # show the size of chunks and whole dataSet
-    print(row.shape)
-    # Show the name of columns in dataSet
-    print(row.columns.tolist())
-    # remove lines without comments
-    removed_no_comment = row[row['FeedbackComment'] != "no comment"]
-    print(removed_no_comment.shape)
-    # change FeedbackStatus field to some meaningful values. 0 -> negative,  1 -> positive, 9-> neutral
-    print(row.FeedbackStatus.unique())
-    row.loc[row.FeedbackStatus == 0, 'FeedbackStatus'] = "negative"
-    row.loc[row.FeedbackStatus == 1, 'FeedbackStatus'] = "positive"
-    row.loc[row.FeedbackStatus == 9, 'FeedbackStatus'] = "neutral"
-    print(row.FeedbackStatus.unique())
-    print(row)
+read = pd .read_stata("data/ebay2_indegrees_comments.dta", chunksize=10, columns=['FeedbackComment', 'FeedbackValue'])
+
+
+def clean(corpus):
+    i = 1
+    for row in corpus:
+        if i < 2:
+            # show the size of chunks and whole dataSet
+            print("Size of DataSet:", row.shape)
+            # Show the name of columns in dataSet
+            print("column names: ", row.columns.tolist())
+            # show the DataSet values
+            print("DataSet values:\n", row)
+            # remove lines without comments
+            row.drop(row[row.FeedbackComment == 'no comment'].index, inplace=True)
+            print(row.shape)
+            # change FeedbackStatus field to some meaningful values. 1 -> negative,  2 -> positive, 8-> neutral
+            print(row.FeedbackValue.unique())
+            row.FeedbackValue.replace([1, 2, 8], ['negative', 'positive', 'neutral'], inplace=True)
+            print(row.FeedbackValue.unique())
+            print(row)
+            i = i + 1
+            return row
+
+
+d = pd.DataFrame()
+d = clean(read)
+print(d)
 
