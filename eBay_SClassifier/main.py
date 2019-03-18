c872835795ea02860713ca3ffe2b31627674d922
Nadi Najafabadi <s.nadi@uu.nl>
2019-03-18 09:46:08 +0100

    Create main.py

---
+++ eBay_SClassifier/main.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import SentimentClass
+from sklearn.metrics import confusion_matrix
+import string
+import de_preprocessing
+import translate
+
+
+pd.set_option('display.expand_frame_repr', False)
+
+
+
+def sentiment(preprocess = False, sentiment_de = False, translate_to_en =False, sentiment_en = False, plot = False):
+        # ***************** Pre-processing *********************************************************************
+        if preprocess==True:
+            data_set = pd.read_stata("test/sample.dta", columns=['FeedbackID', 'FeedbackComment', 'FeedbackValue'])
+            # print(data_set)
+
+            # data_set.FeedbackValue.replace([1, 2, 8], ['negative', 'positive', 'neutral'], inplace=True)
+            # # print(data_set)
+            #
+            #
+            # data_set['Feedback_RemovedPunctuations'] = data_set['FeedbackComment'].apply(
+            # lambda x: ''.join([i for i in x
+            #                    if i not in string.punctuation]))
+            # # print(data_set)
+            #
+            #
+            # data_set['FeedbackComment_lowercase'] = data_set['Feedback_RemovedPunctuations'].str.lower()
+            # print(data_set)
+
+
+            # data_set['FeedbackComment_CorrectSpelling'] = data_set.apply(lambda y: de_preprocessing.check_spell4(y['FeedbackComment_lowercase']), axis=1)
+            # data_set['PFeedbackComment'] = data_set['FeedbackComment_CorrectSelling']
+            # print(data_set)
+            t = data_set['FeedbackComment_lowercase']
+            x = SentimentClass.PreProcess(t)
+            data_set['FeedbackComment_CorrectSpelling'] = x.correct_spell()
+            print(data_set)
+
+            # out = 'test/preprocessed.dta'
+            # data_set.to_stata(out)
+
+        # ***************** German Sentiment *******************************************************************
+        if sentiment_de == True:
+            data_set = pd.read_stata("test/preprocessed.dta", columns=['FeedbackID', 'PFeedbackComment', 'FeedbackValue'])
+            text = data_set['PFeedbackComment']
+            # sentiment_de is an object from SentimentLexDe class
+            sentiment_de = SentimentClass.SentimentLexDe(text)
+            # Calculate sentiment using textblob_de
+            data_set['textblobde_score'], data_set['textblobde_category'] = sentiment_de.sentiment_textblobde()
+            # Calculate sentiment using polyglot
+            data_set['polyglot_score'], data_set['polyglot_category'] = sentiment_de.sentiment_polyglot()
+            print(data_set)
+            outfile = 'test/sentiment_de.dta'
+            data_set.to_stata(outfile)
+
+        # ****************** Translate to English **************************************************************
+        if translate_to_en ==True:
+            data_set = pd.read_stata('test/sentiment_de.dta', columns=['FeedbackID', 'PFeedbackComment',
+                                                                       'FeedbackValue', 'textblobde_score',
+                                                                       'textblobde_category', 'polyglot_score',
+                                                                       'polyglot_category'])
+            data_set['FeedbackComment_en'] = data_set.apply(lambda y:translate.translate_de(y['PFeedbackComment']), axis=1)
+            print(data_set)
+            outfile = 'test/english.dta'
+            data_set.to_stata(outfile)
+
+
+        # ****************** English Sentiment ******************************************************************
+        if sentiment_en == True:
+            data_set = pd.read_stata("test/english.dta",columns=['FeedbackID', 'PFeedbackComment',
+                                                                       'FeedbackValue', 'textblobde_score',
+                                                                       'textblobde_category', 'polyglot_score',
+                                                                       'polyglot_category', 'FeedbackComment_en'])
+            text_en = data_set['FeedbackComment_en']
+            sentiment_en = SentimentClass.SentimentLexEn(text_en)
+            # Calculate sentiment using Afinn lexicon on translated text
+            data_set['afinn_scores'], data_set['afinn_category'] = sentiment_en.sentiment_afinn()
+            # Calculate sentiment using textblob on translated text
+            data_set['textblob_scores'], data_set['textblob_category'] = sentiment_en.sentiment_textblob()
+            print(data_set)
+            outfile = 'test/sentiment_en.dta'
+            data_set.to_stata(outfile)
+
+
+        # ******************* Plot Confiusion Matrix ************************************************************
+        if plot==True:
+            data_set = pd.read_stata("test/sentiment_en.dta",columns=['FeedbackID', 'PFeedbackComment',
+                                                                       'FeedbackValue', 'textblobde_score',
+                                                                       'textblobde_category', 'polyglot_score',
+                                                                       'polyglot_category', 'FeedbackComment_en',
+                                                                      'afinn_scores', 'afinn_category',
+                                                                      'textblob_scores', 'textblob_category'])
+            conf1 = confusion_matrix(data_set['FeedbackValue'], data_set['textblobde_category'])
+            print('Confusion Matrix:\n', conf1)
+            c = SentimentClass.Plot_sentiment()
+            c.plot_confusion_matrix('textblobde', cm=conf1, normalize=False, target_names=['negative', 'neutral', 'positive'],
+                                    cmap='PuBuGn')
+
+            conf2 = confusion_matrix(data_set['FeedbackValue'], data_set['polyglot_category'])
+            print('Confusion Matrix:\n', conf2)
+            c = SentimentClass.Plot_sentiment()
+            c.plot_confusion_matrix('polyglot', cm=conf2, normalize=False, target_names=['negative', 'neutral', 'positive'],
+                                    cmap='BuPu')
+
+            conf3 = confusion_matrix(data_set['FeedbackValue'], data_set['afinn_category'])
+            print('Confusion Matrix:\n', conf3)
+            c = SentimentClass.Plot_sentiment()
+            c.plot_confusion_matrix('afinn', cm=conf3, normalize=False, target_names=['negative', 'neutral', 'positive'],
+                                    cmap='GnBu')
+
+            conf4 = confusion_matrix(data_set['FeedbackValue'], data_set['textblob_category'])
+            print('Confusion Matrix:\n', conf4)
+            c = SentimentClass.Plot_sentiment()
+            c.plot_confusion_matrix('textblob', cm=conf4, normalize=False, target_names=['negative', 'neutral', 'positive'],
+                                    cmap='Purples')
+
+
+
+
+sentiment(preprocess=True, sentiment_de=False, translate_to_en=False, sentiment_en=False, plot=False)
+# data_set = pd.read_stata('data/sentiment2.dta')
+# print(data_set)
